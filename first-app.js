76988cffb73f4e4c3842448a155dd983785d76af
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-14 16:37:19 -0400

    callbacks no promises.

--- first-app.js
+++ first-app.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 
+const fetchData = (callback) => {
+    setTimeout(() => {
+        callback('Done');
+    }, 1500);
+};
+//fs.writeFileSync('./hello.txt', 'hello from nodejs\n');
 
-fs.writeFileSync('./hello.txt', 'hello from nodejs\n');
-
-console.log('hello from node.js');
+setTimeout(() => {
+    console.log('timer is done');
+    fetchData((text) => {
+        console.log(text);
+    });
+}, 100);
