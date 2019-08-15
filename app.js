ad3b404a073c3fe1b61cc0fd8989f0af15de6d66
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-15 11:42:17 -0400

    send form. User submits form, redirects to /message

--- app.js
+++ app.js
@@ -10,8 +10,23 @@ function rqListener(req, res) {
 
 const server = http.createServer((req, res) => {
     // execute things for incoming requests
-    console.log('**** ', req.url, req.method, req.headers);
-    // process.exit(); // kills server
+
+    const url = req.url;
+
+    // if root path
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        
+        // Giving "input" a NAME, causes message to be sent to server with form POST.
+        res.write('<body><form action="/message" method="POST"> \
+                        <input type="text" name="message"><button type="submit">Send</button></input> \
+                    </form></body>');
+        res.write('</html>');
+        return res.end(); // return from anonymous function. Cannot continue this function.
+    }
+    // this runs on /message because not in IF.
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>First page</title></head>');
