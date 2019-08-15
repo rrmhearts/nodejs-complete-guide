f90265013866585d6cb5dff63d91cfe255735890
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-15 09:21:41 -0400

    promises overview

--- first-app.js
+++ first-app.js
@@ -1,15 +1,28 @@
 const fs = require('fs');
 
-const fetchData = (callback) => {
-    setTimeout(() => {
-        callback('Done');
-    }, 1500);
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done');
+        }, 1500);
+    });
+
+    return promise;    
 };
 //fs.writeFileSync('./hello.txt', 'hello from nodejs\n');
 
 setTimeout(() => {
     console.log('timer is done');
-    fetchData((text) => {
+    fetchData()
+    .then(text => {
         console.log(text);
+        return fetchData();
+        /*
+        fetchData().then(text2 => {
+            console.log(text2);
+        })*/
+    })
+    .then(text2 => {
+        console.log(text2);
     });
 }, 100);
