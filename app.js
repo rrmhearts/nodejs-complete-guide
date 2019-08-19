650e49bb71227b2aa165fb2fb6f7e7016ea6de2e
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-19 13:20:21 -0400

    parse post request into file. logic behind express

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //const fs = require('fs');
 const http = require('http');
+const fs = require('fs');
 
 // runs for every incoming request
 function rqListener(req, res) {
@@ -12,6 +13,7 @@ const server = http.createServer((req, res) => {
     // execute things for incoming requests
 
     const url = req.url;
+    const method = req.method;
 
     // if root path
     if (url === '/') {
@@ -25,6 +27,25 @@ const server = http.createServer((req, res) => {
                     </form></body>');
         res.write('</html>');
         return res.end(); // return from anonymous function. Cannot continue this function.
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data',(chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        //fs.writeFileSync('message.txt', 'Dummy');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+
     }
     // this runs on /message because not in IF.
     res.setHeader('Content-Type', 'text/html');
