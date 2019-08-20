2d25c72e3e70855c5962d7d9e269187c948f9415
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-20 15:50:48 -0400

    basic how to Express

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
+const app = express(); // Valid request handler.
+/*
+ **** How To Express ****
+    Request
+    --->
+    Middleware 1
+    --->
+    Middleware 2
+    --->
+    Response
+*/
+// runs on every request
+app.use((req, res, next /*func*/) => {
+    console.log('In the middleware');
+    next(); // go to next middleware 
+}); // add a new middleware function
 
-const server = http.createServer(routes);
+app.use((req, res, next /*func*/) => {
+    console.log('In ANOTHER middleware');
+    res.send('<h1>Hello from Express!</h1>')
+}); // add a new middleware function
+
+const server = http.createServer(app);
 
 server.listen(3000);
