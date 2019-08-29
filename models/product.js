251e3c2f154c46691f969c90918121d98985216a
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-29 10:37:55 -0400

    File based Model.

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(title) {
@@ -6,11 +7,38 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'models',
+            'data',
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
     // Static puts method on the class itself.
-    static fetchAll() {
-        return products;
+    static fetchAll(callback) {
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'models',
+            'data',
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                callback([]);
+            }
+            callback(JSON.parse(fileContent));
+        });
     }
-}
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,7 @@
-const path = require('path');
-
 const express = require('express');
 const router = express.Router();
 
 const productController = require('../controllers/products');
-const rootDir = require('../util/path');
 /*
     Mini-express app which can be exported.
 */
