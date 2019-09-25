397b3b1d2751c2503e440a94e23d56714fe1fa39
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-09-25 11:34:30 -0400

    Replace Sequelize with MongoDb. Fetch products.

--- controllers/errors.js
+++
@@ -1,6 +0,0 @@
-exports.getError404 = (req, res, next) => {
-    res.status(404).render('404', {
-        pageTitle: 'Page Not Found',
-        path: '/404',
-    }); // Passing data into templating engine DOESN't change
-};
