234fb1dd36a6112de598781030fba6631aa64cb0
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-12-30 10:45:41 -0500

    store session in MongoDB for security and reliability.

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const app = express();
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`+
+                    '/shop'; // using shop db for both.
 
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+}); // constructor
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -23,8 +30,10 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'secret string', resave: false/*only if something changes*/,
+  secret: 'secret string', /*signing hash for id in cookie*/
+  resave: false, /*only if something changes*/
   saveUninitialized: false, /*cookie: {maxAge:... expires...}*/
+  store: store, /* store session data in mongodb! (FOR PRODUCTION ~~ more secure, will not overload memory)*/
 }));
 
 /* Middle ware to login a user */
@@ -49,8 +58,7 @@ app.use(authRoutes);
 // If request does not have a route above!
 app.use(errorController.get404);
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`+
-'/shop?retryWrites=true&w=majority')
+mongoose.connect(MONGODB_URI+'?retryWrites=true&w=majority')
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
