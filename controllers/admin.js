bba77da3cb5f05279c13414c8bd98056d3ed551e
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2020-01-02 13:46:06 -0500

    Lock down route, not scalable.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,11 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
+  // If not logged in, don't allow page load. Lock down route.
+  if (!req.session.isLoggedIn) {
+    return res.redirect('/login');
+  }
+
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
