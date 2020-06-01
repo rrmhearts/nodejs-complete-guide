6aba57f1119d6899def5b82d12333b4981325460
Ryan <rrmhearts@gmail.com>
2020-06-01 13:52:51 -0400

    Add/Edit product validation.

--- routes/shop.js
+++ routes/shop.js
@@ -8,16 +8,19 @@ const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
+
 router.get('/products', shopController.getProducts);
 
-/*
-    In process from switching from Sequelize/SQL to Mongoose/MongoDB
-*/
 router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', isAuth, shopController.getCart);
+
 router.post('/cart', isAuth, shopController.postCart);
+
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
+
 router.post('/create-order', isAuth, shopController.postOrder);
+
 router.get('/orders', isAuth, shopController.getOrders);
 
 module.exports = router;
